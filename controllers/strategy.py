c0d4eed29b8c7497dbf46e1702b9938aef337034
rharuperi <r.haruperi@hotmail.com>
2025-02-07 15:55:38 +0200

    Added ma_trend_willpct_strategy in strategy.py

--- controllers/strategy.py
+++ controllers/strategy.py
@@ -1,4 +1,7 @@
-from technicals import *
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from .technicals import *
 
 
 
@@ -20,3 +23,34 @@ def random(symbol):
         return f"{symbol} Sell"
     else:
         return f"{symbol} Neutral"
+
+
+
+def ma_trend_willpct_strategy(symbol, df, fast_sma_period=g_fast_ma,
+                              slow_sma_period=g_slow_ma, williamsR_period=g_willpct_period):
+    """
+    Implements a trading strategy based on moving average trends and Williams %R oscillator.
+
+    Args:
+        symbol (str): The trading symbol to analyze.
+        :param df:
+        :param williamsR_period:
+        :param slow_sma_period:
+        :param symbol:
+        :param fast_sma_period:
+
+    Returns:
+        str: A string indicating the generated signal: "<symbol> Buy", "<symbol> Sell", or "<symbol> Neutral".
+    """
+    df = calculate_ma_trend_momentum_signals(df, fast_sma_period, slow_sma_period, williamsR_period)
+
+    # Get current signal and time
+    signal = df["signal"].iloc[-1]
+    curr_time = df.index[-1]
+
+    if signal == 1:
+        return f"{curr_time} {symbol} Buy"
+    elif signal == -1:
+        return f"{curr_time} {symbol} Sell"
+    else:
+        return f"{curr_time} {symbol} Neutral"
