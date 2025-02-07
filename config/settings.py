633b96cff7573afce3cdf60db27e6ddabdf952bd
rharuperi <r.haruperi@hotmail.com>
2025-02-07 10:14:45 +0200

    added enable_all_symbols function

--- config/settings.py
+++ config/settings.py
@@ -2,13 +2,26 @@
 ##                            GLOBAL SETTINGS - VARIABLES (g_)                              ##
 ##############################################################################################
 from datetime import timedelta, datetime
-#from controllers.data import *
+from controllers.data import *
 
 
 #--------------------------------------- Credentials data -----------------------------------#
 
 
 g_credentials_filepath = "./config/credentials.json"
+
+
+#--------------------------------------- Symbols Data -----------------------------------#
+
+
+g_symbols_forex = ["AUDCAD", "AUDCHF", "AUDJPY", "AUDNZD", "AUDUSD",
+    "CADCHF", "CADJPY", "CHFJPY",
+    "EURAUD", "EURCAD", "EURCHF", "EURGBP", "EURJPY", "EURNZD", "EURUSD",
+    "GBPAUD", "GBPCAD", "GBPCHF", "GBPJPY", "GBPNZD", "GBPUSD",
+    "NZDCAD", "NZDCHF", "NZDJPY", "NZDUSD",
+    "USDCHF", "USDCAD", "USDJPY"]
+g_symbols_commodities = ["XAUUSD", "XAUEUR", "XAUGBP", "XAUJPY", "XAUAUD", "XAUCHF", "XAGUSD"]
+g_symbols_indices = ["US500", "US30", "UK100", "GER40", "NAS100", "USDX", "EURX"]
 
 
 #--------------------------------------- Data variables -----------------------------------------#
@@ -62,21 +75,11 @@ g_strength_lookback_period = 144
 #--------------------------------------- GLOBAL INIT -----------------------------------#
 
 
-g_project_settings = get_project_settings(g_settings_filepath)                # Import settings
-g_init_mt5 = initialize_mt5(g_project_settings)                               # Start MT5
-g_init_symbols = enable_all_symbols(g_project_settings)                       # Initialize Symbols
+g_project_settings = get_project_settings(g_credentials_filepath)              # Import settings
+g_init_mt5 = initialize_mt5(g_project_settings)                                # Start MT5
+g_init_symbols = enable_all_symbols(g_symbols_forex)                       # Initialize Symbols
 g_token = g_project_settings['telegram']['token']
 g_chat_id = g_project_settings['telegram']['chat_id']
 
 
-#--------------------------------------- Symbols Data -----------------------------------#
 
-
-g_symbols_forex = ["AUDCAD", "AUDCHF", "AUDJPY", "AUDNZD", "AUDUSD",
-    "CADCHF", "CADJPY", "CHFJPY",
-    "EURAUD", "EURCAD", "EURCHF", "EURGBP", "EURJPY", "EURNZD", "EURUSD",
-    "GBPAUD", "GBPCAD", "GBPCHF", "GBPJPY", "GBPNZD", "GBPUSD",
-    "NZDCAD", "NZDCHF", "NZDJPY", "NZDUSD",
-    "USDCHF", "USDCAD", "USDJPY"],
-g_symbols_commodities = ["XAUUSD", "XAUEUR", "XAUGBP", "XAUJPY", "XAUAUD", "XAUCHF", "XAGUSD"],
-g_symbols_indices = ["US500", "US30", "UK100", "GER40", "NAS100", "USDX", "EURX"]
--- controllers/data.py
+++ controllers/data.py
@@ -3,6 +3,7 @@
 ##############################################################################################
 import json
 import os
+import MetaTrader5 as mt5
 
 
 #----------------------------------------------------------------------------------------------#
@@ -37,3 +38,102 @@ def get_project_settings(import_filepath):
 
 
 #----------------------------------------------------------------------------------------------#
+
+
+def initialize_mt5(project_settings):
+    """
+    Initializes and logs in to the MetaTrader 5 (MT5) platform.
+
+    This function utilizes the provided project settings to initialize 
+    the MT5 terminal and log in using the credentials and server details.
+
+    Args:
+        project_settings (dict): A dictionary containing MT5 login information, including:
+            - 'username' (str): The username for login.
+            - 'password' (str): The password for the MT5 account.
+            - 'server' (str): The server name used for the MT5 connection.
+            - 'pathway' (str): The file path to the MT5 terminal executable.
+
+    Returns:
+        bool: True if MT5 is initialized and login is successful, False otherwise.
+    """
+    # Attempt to start MT5
+    # Ensure that all variables are set to the correct type
+    username = project_settings['mt5']['username']
+    username = int(username)
+    password = project_settings['mt5']['password']
+    server = project_settings['mt5']['server']
+    pathway = project_settings['mt5']['pathway']
+
+    # Attempt to initialize MT5
+    try:
+        mt5_init = mt5.initialize(
+            login=username,
+            password=password,
+            server=server,
+            path=pathway
+        )
+    except Exception as e:
+        print(f"Error initializing MetaTrader 5: {e}")
+        # I cover more advanced error handling in other courses, which are useful for troubleshooting
+        mt5_init = False
+
+    # If MT5 initialized, attempt to log in to MT5
+    mt5_login = False
+    if mt5_init:
+        try:
+            mt5_login = mt5.login(
+                login=username,
+                password=password,
+                server=server
+            )
+        except Exception as e:
+            print(f"Error logging into MetaTrader 5: {e}")
+            mt5_login = False
+
+    # Return the outcome to the user
+    if mt5_login:
+        return True
+    # Default fail condition of not logged in
+    return False
+
+
+#------------------------------------------------------------------------------------------------------#
+
+
+def enable_all_symbols(symbols):
+    """
+    Enables all specified symbols in the MetaTrader 5 platform.
+
+    Args:
+        symbols (list): A list of symbol names to be enabled.
+
+    Returns:
+        bool: True if all symbols were successfully enabled, False otherwise.
+    """
+
+    # Get all symbols from MT5
+    mt5_symbols = mt5.symbols_get()
+
+    # Create a set of all MT5 symbols for quick lookup
+    mt5_symbols_set = {symbol.name for symbol in mt5_symbols}
+
+    # Iterate through my_symbols to check if they exist in mt5_symbols
+    for symbol in symbols:
+        if symbol in mt5_symbols_set:
+            # Attempt to initialize/enable the symbol
+            result = mt5.symbol_select(symbol, True)
+            if not result:
+                print(f"Failed to enable {symbol}.")
+        else:
+            print(f"{symbol} does not exist in MT5 symbols. Please update symbol name.")
+            return False
+
+    # Default to return True
+    return True
+
+
+
+#------------------------------------------------------------------------------------------------------#
+
+
