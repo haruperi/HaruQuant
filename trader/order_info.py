4394cff6a163d14e1c4bd93c29875358c6e0fb19
rharu <r.haruperi@hotmail.com>
2025-03-06 20:57:13 +0200

    cleaned order_info.py

--- trader/order_info.py
+++ trader/order_info.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from .order_enums import OrderType, OrderState, OrderTypeFilling, OrderTypeTime
 
 
-
 class OrderInfo:
     """
     Class for access to order info.
@@ -20,6 +19,8 @@ class OrderInfo:
         self.m_price_open = 0.0
         self.m_stop_loss = 0.0
         self.m_take_profit = 0.0
+        self.m_comment = ""
+        self.m_magic = 0
 
     def get_ticket(self):
         """Get order ticket"""
@@ -27,148 +28,184 @@ class OrderInfo:
 
     def get_time_setup(self):
         """Get order setup time"""
-        return datetime.fromtimestamp(mt5.order_info(self.m_ticket).time_setup)
-
-    def get_time_setup_msc(self):
-        """Get order setup time in milliseconds"""
-        return mt5.order_info(self.m_ticket).time_setup_msc
+        orders = mt5.orders_get(ticket=self.m_ticket)
+        if orders is None or len(orders) == 0:
+            raise Exception(f"Order with ticket {self.m_ticket} not found.")
+        return datetime.fromtimestamp(orders[0].time_setup)
 
     def get_time_done(self):
         """Get order execution or cancellation time"""
-        return datetime.fromtimestamp(mt5.order_info(self.m_ticket).time_done)
-
-    def get_time_done_msc(self):
-        """Get order execution or cancellation time in milliseconds"""
-        return mt5.order_info(self.m_ticket).time_done_msc
+        orders = mt5.orders_get(ticket=self.m_ticket)
+        if orders is None or len(orders) == 0:
+            raise Exception(f"Order with ticket {self.m_ticket} not found.")
+        return datetime.fromtimestamp(orders[0].time_done)
 
     def get_order_type(self):
         """Get order type"""
-        return OrderType(mt5.order_info(self.m_ticket).type)
+        orders = mt5.orders_get(ticket=self.m_ticket)
+        if orders is None or len(orders) == 0:
+            raise Exception(f"Order with ticket {self.m_ticket} not found.")
+        else:
+            self.m_type = OrderType(orders[0].type)
+        return self.m_type
 
     def get_type_description(self):
         """Get order type as string"""
-        order_type = self.OrderType()
-        return self.FormatType("", order_type.value)
+        if self.m_type is None:
+            self.get_order_type()  # This will update self.m_type from mt5.orders_get
+        return self.format_type(self.m_type.value)
 
     def get_state(self):
         """Get order state"""
-        return OrderState(mt5.order_info(self.m_ticket).state)
+        orders = mt5.orders_get(ticket=self.m_ticket)
+        if orders is None or len(orders) == 0:
+            raise Exception(f"Order with ticket {self.m_ticket} not found.")
+        else:
+            self.m_state = OrderState(orders[0].state)
+        return self.m_state
 
     def get_state_description(self):
         """Get order state as string"""
-        state = self.State()
-        return self.FormatStatus("", state.value)
+        if self.m_state is None:
+            self.get_state()  # This will update self.m_state from mt5.orders_get
+            # Check again if m_state is still None
+            if self.m_state is None:
+                return "Unknown order state: order details not found."
+        return self.format_status(self.m_state.value)
 
     def get_time_expiration(self):
         """Get order expiration time"""
-        expiration = mt5.order_info(self.m_ticket).time_expiration
-        return datetime.fromtimestamp(expiration) if expiration > 0 else None
+        orders = mt5.orders_get(ticket=self.m_ticket)
+        if orders is None or len(orders) == 0:
+            raise Exception(f"Order with ticket {self.m_ticket} not found.")
+        expiration = orders[0].time_expiration
+        if expiration > 0:
+            self.m_expiration = datetime.fromtimestamp(expiration)
+
+        return self.m_expiration
 
     def get_type_filling(self):
         """Get order filling type"""
-        return OrderTypeFilling(mt5.order_info(self.m_ticket).type_filling)
+        orders = mt5.orders_get(ticket=self.m_ticket)
+        if orders is None or len(orders) == 0:
+            raise Exception(f"Order with ticket {self.m_ticket} not found.")
+        return OrderTypeFilling(orders[0].type_filling)
 
     def get_type_filling_description(self):
         """Get order filling type as string"""
-        filling_type = self.TypeFilling()
-        return self.FormatTypeFilling("", filling_type.value)
+        filling_type = self.get_type_filling()
+        return self.format_type_filling(filling_type.value)
 
     def get_type_time(self):
         """Get order lifetime type"""
-        return OrderTypeTime(mt5.order_info(self.m_ticket).type_time)
+        orders = mt5.orders_get(ticket=self.m_ticket)
+        if orders is None or len(orders) == 0:
+            raise Exception(f"Order with ticket {self.m_ticket} not found.")
+        return OrderTypeTime(orders[0].type_time)
 
     def get_type_time_description(self):
         """Get order lifetime type as string"""
-        time_type = self.TypeTime()
-        return self.FormatTypeTime("", time_type.value)
+        time_type = self.get_type_time()
+        return self.format_type_time(time_type.value)
+
+    def set_magic(self, magic):
+        """Set order magic number"""
+        self.m_magic = magic
 
     def get_magic_number(self):
         """Get order magic number"""
-        return mt5.order_info(self.m_ticket).magic
+        orders = mt5.orders_get(ticket=self.m_ticket)
+        if orders is None or len(orders) == 0:
+            raise Exception(f"Order with ticket {self.m_ticket} not found.")
+        return orders[0].magic
 
     def get_position_id(self):
         """Get position identifier"""
-        return mt5.order_info(self.m_ticket).position_id
+        orders = mt5.orders_get(ticket=self.m_ticket)
+        if orders is None or len(orders) == 0:
+            raise Exception(f"Order with ticket {self.m_ticket} not found.")
+        return orders[0].position_id
 
     def get_position_by_id(self):
         """Get identifier of an opposite position"""
-        return mt5.order_info(self.m_ticket).position_by_id
+        orders = mt5.orders_get(ticket=self.m_ticket)
+        if orders is None or len(orders) == 0:
+            raise Exception(f"Order with ticket {self.m_ticket} not found.")
+        return orders[0].position_by_id
 
     def get_volume_initial(self):
         """Get initial order volume"""
-        return mt5.order_info(self.m_ticket).volume_initial
+        orders = mt5.orders_get(ticket=self.m_ticket)
+        if orders is None or len(orders) == 0:
+            raise Exception(f"Order with ticket {self.m_ticket} not found.")
+        return orders[0].volume_initial
 
     def get_volume_current(self):
         """Get current order volume"""
-        return mt5.order_info(self.m_ticket).volume_current
+        orders = mt5.orders_get(ticket=self.m_ticket)
+        if orders is None or len(orders) == 0:
+            raise Exception(f"Order with ticket {self.m_ticket} not found.")
+        else:
+            self.m_volume_curr = orders[0].volume_current
+        return self.m_volume_curr
 
     def get_open_price(self):
         """Get order price"""
-        return mt5.order_info(self.m_ticket).price_open
+        orders = mt5.orders_get(ticket=self.m_ticket)
+        if orders is None or len(orders) == 0:
+            raise Exception(f"Order with ticket {self.m_ticket} not found.")
+        else:
+            self.m_price_open = orders[0].price_open
+        return self.m_price_open
 
     def get_stop_loss(self):
         """Get order stop loss"""
-        return mt5.order_info(self.m_ticket).sl
+        orders = mt5.orders_get(ticket=self.m_ticket)
+        if orders is None or len(orders) == 0:
+            raise Exception(f"Order with ticket {self.m_ticket} not found.")
+        else:
+            self.m_stop_loss = orders[0].sl
+        return self.m_stop_loss
 
     def get_take_profit(self):
         """Get order take profit"""
-        return mt5.order_info(self.m_ticket).tp
+        orders = mt5.orders_get(ticket=self.m_ticket)
+        if orders is None or len(orders) == 0:
+            raise Exception(f"Order with ticket {self.m_ticket} not found.")
+        else:
+            self.m_take_profit = orders[0].tp
+        return self.m_take_profit
 
     def get_current_price(self):
         """Get current price of the order symbol"""
-        symbol = self.Symbol()
-        return mt5.symbol_info_tick(symbol).ask if self.OrderType().value % 2 == 0 else mt5.symbol_info_tick(symbol).bid
+        symbol = self.get_symbol()
+        tick = mt5.symbol_info_tick(symbol)
 
-    def get_stop_limit_price(self):
-        """Get the limit order price for the StopLimit order"""
-        return mt5.order_info(self.m_ticket).price_stoplimit
+        if self.m_type is None:
+            self.get_order_type()
+
+        return tick.ask if self.m_type.value % 2 == 0 else tick.bid
 
     def get_symbol(self):
         """Get order symbol"""
-        return mt5.order_info(self.m_ticket).symbol
+        orders = mt5.orders_get(ticket=self.m_ticket)
+        if orders is None or len(orders) == 0:
+            raise Exception(f"Order with ticket {self.m_ticket} not found.")
+        return orders[0].symbol
+
+    def set_comment(self, comment):
+        """Set order comment"""
+        self.m_comment = comment
 
     def get_comment(self):
         """Get order comment"""
-        return mt5.order_info(self.m_ticket).comment
+        orders = mt5.orders_get(ticket=self.m_ticket)
+        if orders is None or len(orders) == 0:
+            raise Exception(f"Order with ticket {self.m_ticket} not found.")
+        return orders[0].comment
 
-    def get_external_id(self):
-        """Get order identifier in an external trading system"""
-        return mt5.order_info(self.m_ticket).external_id
-
-    def is_info_integer(self, prop_id, var):
-        """Get the value of an integer property"""
-        try:
-            result = mt5.order_get_integer(self.m_ticket, prop_id)
-            if result is not None:
-                var = result
-                return True
-            return False
-        except:
-            return False
-
-    def is_info_double(self, prop_id, var):
-        """Get the value of a double property"""
-        try:
-            result = mt5.order_get_double(self.m_ticket, prop_id)
-            if result is not None:
-                var = result
-                return True
-            return False
-        except:
-            return False
-
-    def is_info_string(self, prop_id, var):
-        """Get the value of a string property"""
-        try:
-            result = mt5.order_get_string(self.m_ticket, prop_id)
-            if result is not None:
-                var = result
-                return True
-            return False
-        except:
-            return False
-
-    def get_type(self, str_val, type_val):
+    @staticmethod
+    def format_type(type_val):
         """Format the order type as string"""
         order_types = {
             0: "Buy",
@@ -183,7 +220,8 @@ class OrderInfo:
         }
         return order_types.get(type_val, "Unknown")
 
-    def get_status(self, str_val, status):
+    @staticmethod
+    def format_status(status):
         """Format the order status as string"""
         order_states = {
             0: "Started",
@@ -199,7 +237,8 @@ class OrderInfo:
         }
         return order_states.get(status, "Unknown")
 
-    def get_type_filling_str(self, str_val, type_val):
+    @staticmethod
+    def format_type_filling(type_val):
         """Format the order filling type as string"""
         filling_types = {
             0: "Fill or Kill",
@@ -208,7 +247,8 @@ class OrderInfo:
         }
         return filling_types.get(type_val, "Unknown")
 
-    def get_type_time_str(self, str_val, type_val):
+    @staticmethod
+    def format_type_time(type_val):
         """Format the order lifetime as string"""
         time_types = {
             0: "GTC",
@@ -218,22 +258,23 @@ class OrderInfo:
         }
         return time_types.get(type_val, "Unknown")
 
-    def get_order(self, str_val):
-        """Format the order description"""
-        order_info = mt5.order_info(self.m_ticket)
-        if order_info is None:
+    def get_order(self):
+        """Get order description in string format"""
+        orders = mt5.orders_get(ticket=self.m_ticket)
+        if orders is None:
             return "Invalid order"
 
-        order_type = self.get_type("", order_info.type)
-        symbol = order_info.symbol
-        volume = order_info.volume_current
-        price = order_info.price_open
-        sl = order_info.sl
-        tp = order_info.tp
+        order_type = self.get_type_description()
+        symbol = orders[0].symbol
+        volume = orders[0].volume_current
+        price = orders[0].price_open
+        sl = orders[0].sl
+        tp = orders[0].tp
 
         return f"{order_type} {symbol} {volume} at {price} sl: {sl} tp: {tp}"
 
-    def get_order_price(self, str_val, price_order, price_trigger, digits):
+    @staticmethod
+    def format_price(price_order, price_trigger, digits):
         """Format the order price"""
         if price_trigger:
             return f"{price_order:.{digits}f} ({price_trigger:.{digits}f})"
@@ -241,16 +282,6 @@ class OrderInfo:
 
     def select(self, ticket=None):
         """Select an order for further work"""
-        if ticket is None:
-            # Try to select by current ticket
-            if self.m_ticket == 0:
-                return False
-            ticket = self.m_ticket
-
-        order_info = mt5.order_info(ticket)
-        if order_info is None:
-            return False
-
         self.m_ticket = ticket
         self.store_state()
         return True
@@ -265,35 +296,32 @@ class OrderInfo:
 
     def store_state(self):
         """Store order state"""
-        order_info = mt5.order_info(self.m_ticket)
-        if order_info is None:
+        orders_get = mt5.orders_get(self.m_ticket)
+        if orders_get is None:
             return
 
-        self.m_type = order_info.type
-        self.m_state = order_info.state
-        self.m_expiration = order_info.time_expiration
-        self.m_volume_curr = order_info.volume_current
-        self.m_price_open = order_info.price_open
-        self.m_stop_loss = order_info.sl
-        self.m_take_profit = order_info.tp
+        self.m_type = orders_get.type
+        self.m_state = orders_get.state
+        self.m_expiration = orders_get.time_expiration
+        self.m_volume_curr = orders_get.volume_current
+        self.m_price_open = orders_get.price_open
+        self.m_stop_loss = orders_get.sl
+        self.m_take_profit = orders_get.tp
 
     def check_state(self):
         """Check if order state has changed Returns True if the order state has changed, False otherwise"""
-        order_info = mt5.order_info(self.m_ticket)
-        if order_info is None:
+        orders_get = mt5.orders_get(self.m_ticket)
+        if orders_get is None:
             return False
-            
-        if (self.m_type == order_info.type and
-            self.m_state == order_info.state and
-            self.m_expiration == order_info.time_expiration and
-            self.m_volume_curr == order_info.volume_current and
-            self.m_price_open == order_info.price_open and
-            self.m_stop_loss == order_info.sl and
-            self.m_take_profit == order_info.tp):
+
+        if (self.m_type == orders_get.type and
+                self.m_state == orders_get.state and
+                self.m_expiration == orders_get.time_expiration and
+                self.m_volume_curr == orders_get.volume_current and
+                self.m_price_open == orders_get.price_open and
+                self.m_stop_loss == orders_get.sl and
+                self.m_take_profit == orders_get.tp):
             return False
-            
+
         # State has changed
         return True
-
-
-
