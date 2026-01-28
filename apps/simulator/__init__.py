bf6f68ad3cf31785c92453060b414854bb689b87
rharu <r.haruperi@hotmail.com>
2026-01-28 10:09:02 +0200

    refractor : Deleated files in refractoring

--- apps/simulator/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Local trade simulator module aligned with MT5 conventions."""
-
-from .engine import TradeSimulator
-from .gateway import TradeGateway
-from .market_data import MarketDataStore
-
-__all__ = ["TradeSimulator", "MarketDataStore", "TradeGateway"]
